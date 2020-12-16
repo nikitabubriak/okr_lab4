637372379518f45c840c5290aad8acbbed8223c1
Nikita Bubriak <31805409+nikitabubriak@users.noreply.github.com>
2020-12-16 13:21:15 +0200

    Fix links

--- js/index.js
+++ js/index.js
@@ -6,23 +6,34 @@ import TemplateProcessor from './templateProcessor.js';
 // let loader = document.getElementById("loader");
 // loader.classList.toggle("hidden");
 
-const router = new Router();
-const templateProcessor = new TemplateProcessor();
-const client = new Client();
+const render = () =>
+{
+    const router = new Router();
+    const templateProcessor = new TemplateProcessor();
+    const client = new Client();
 
-const { viewName, endpointName } = router.getCurrentState();
+    const { viewName, endpointName } = router.getCurrentState();
 
-let view;
+    let view;
 
-import(`./views/${viewName}.js`)
-    .then((viewModule) =>  
-    {
-        view = viewModule.default;
-        return client.getData(endpointName);
-    })
-    
-    .then((data) => 
-    {
-        templateProcessor.render(view(data));
-    });
+    import(`./views/${viewName}.js`)
+        .then((viewModule) =>  
+        {
+            view = viewModule.default;
+            return client.getData(endpointName);
+        })
+        
+        .then((data) => 
+        {
+            templateProcessor.render(view(data));
+        });
+}
 
+render();
+window.addEventListener('hashchange', render);
+window.onhashchange = render();
+
+
+// if ("onhashchange" in window) {  
+//     alert("The browser supports the hashchange event!");  
+//   }  
