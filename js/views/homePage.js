da83b709ada44dbd2f997d2dc4a2234aaa6f9a0e
Nikita Bubriak <31805409+nikitabubriak@users.noreply.github.com>
2020-12-17 12:55:20 +0200

    Rewrite homePage view

--- js/views/homePage.js
+++ js/views/homePage.js
@@ -1,19 +1,42 @@
 const view = (db) => 
-`
-<div class = "offer">
-    <a class = "offer-link" href = "#offer/${db.offers[0].url}">
-        <img src = "${db.offers[0].image}" alt = "${db.offers[0].name}">
-        </a>
-</div>
+{
+    let homePage = 
+    `
+    <div class = "offer">
+        <a class = "offer-link" href = "#offer/${db.offers[0].url}">
+            <img src = "${db.offers[0].image}" alt = "${db.offers[0].name}">
+            </a>
+    </div>
+    `;
 
-<div class = "products-container">
+    // homePage +=
+    // `
+    // <div class = "products-container">
+    //     <h2>Featured products</h2><br>
+    //     <div class = "products-grid">
+    // `;
+    
+    let featured = [];
+
+    db.catalog[1].map((product) =>
+    {
+        for (let i = 0; i < db.featuredProducts.length; i++)
+        {
+            if (product.ID === db.featuredProducts[i])
+            {
+                featured.push(product);
+            }
+        }
+    });
+
+    homePage += 
+    `
+    <div class = "products-container">
     <h2>Featured products</h2><br>
     <div class = "products-grid">
         ${
-            db.catalog[1].map((product) =>
-            {for (let i = 0; i < db.featuredProducts.length; i++)
-            {if (product.ID === db.featuredProducts[i]) 
-            {`
+            featured.map((product) =>
+            `
             <div class = "product-tile">
                 <a class = "product-link" href = "#products/${product.url}">
                     <img src = "${product.image}" alt = "${product.name}">
@@ -23,10 +46,37 @@ const view = (db) =>
                     <button class = "price-add-to-cart-button">${product.price} ₴</button>
                 </a>
             </div>
-            `}}}).join("")
+            `).join("")
         }
+        </div>
     </div>
-</div>
-`;
+    `;
+    
+
+    // db.catalog[1].map((product) =>
+    // {for (let i = 0; i < db.featuredProducts.length; i++)
+    // {if (product.ID === db.featuredProducts[i]) 
+    // {`
+    // <div class = "product-tile">
+    //     <a class = "product-link" href = "#products/${product.url}">
+    //         <img src = "${product.image}" alt = "${product.name}">
+    //         <br><br><span class = "product-name">${product.name}</span>
+    //     </a>
+    //     <a class = "add-to-cart-link" href = "#cart">
+    //         <button class = "price-add-to-cart-button">${product.price} ₴</button>
+    //     </a>
+    // </div>
+    // `}}}).join("")
+
+    
+    // homePage +=
+    // `
+    // </div>
+    // </div>
+    // `;
+
+    return homePage;
+}
+
 
 export default view;
--- js/views/offerPage.js
+++ js/views/offerPage.js
@@ -2,17 +2,6 @@ const view = (catalog) =>
 `
 <p>OFFER PAGE</p>
 
-<div id = "categories">
-    ${
-        catalog[0].map((category) => 
-        `
-        <div class = "category-item">
-            <span class = "category-name">${category.name}</span>
-            <span class = "category-description">${category.description}</span>
-        </div>
-        `).join("")
-    }
-</div>
 `;
 
 export default view;
