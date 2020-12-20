363fe0e70e4f13a1fe922ca0bf51d7e1f20236e3
Nikita Bubriak <31805409+nikitabubriak@users.noreply.github.com>
2020-12-20 17:29:18 +0200

    Update orderManager.js

--- js/orderManager.js
+++ js/orderManager.js
@@ -3,11 +3,13 @@
 
 const cartAddProduct = () => 
 {
-    //this.removeEventListener("click", cartAddProduct);
+    let btn = document.getElementById("add-to-cart-btn");
     let cart = JSON.parse(localStorage.getItem("cart"));
     if (!cart) cart = {};
-    let id = this.value;
+    let id = btn.value;
     let count = 2;//document.getElementById("product-count").value;
     cart[id] = count;
     localStorage.setItem("cart", JSON.stringify(cart));
+    //btn.removeEventListener("click", cartAddProduct);
+
 }
