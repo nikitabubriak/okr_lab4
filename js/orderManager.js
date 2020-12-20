09be19e0a0e6b85db48c6dcda2291fc958c3c8ae
Nikita Bubriak <31805409+nikitabubriak@users.noreply.github.com>
2020-12-20 19:48:39 +0200

    Add countProducts() and total-count

--- js/orderManager.js
+++ js/orderManager.js
@@ -1,17 +1,26 @@
 //cart
 //order
 
+const countProducts = () =>
+{
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    if (!cart) cart = {};
+    let count = 0;
+    for (let num of Object.entries(cart)) count += num;
+    document.getElementById("total-count").textContent = count;
+}
+
 const cartAddProduct = () => 
 {
     let count = document.getElementById("count").value;
-    if (count = "" || !count) return;
+    if (count == "" || !count) return;
     let btn = document.getElementById("add-to-cart-btn");
     let cart = JSON.parse(localStorage.getItem("cart"));
     if (!cart) cart = {};
     let id = btn.value;
     cart[id] = count;
     localStorage.setItem("cart", JSON.stringify(cart));
-
+    countProducts();
 }
 
 const cartRemoveProduct = (id) =>
@@ -20,10 +29,12 @@ const cartRemoveProduct = (id) =>
     delete cart[id];
     localStorage.setItem("cart", JSON.stringify(cart));
     window.dispatchEvent(new HashChangeEvent("hashchange"));
+    countProducts();
     // localStorage.removeItem(`"cart.${id}"`);
 }
 
 const cartClear = () =>
 {
     localStorage.removeItem("cart");
+    countProducts();
 }
