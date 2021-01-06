7c5414c64962bb80059f1923be715399976204ac
Nikita Bubriak <31805409+nikitabubriak@users.noreply.github.com>
2021-01-07 00:42:10 +0200

    Update orderManager.js

--- js/orderManager.js
+++ js/orderManager.js
@@ -71,8 +71,7 @@ async function submitOrder ()
         total: JSON.parse(localStorage.getItem("totalPrice"))
     }
 
-    //try
-    //{
+    
     const submitResponse = await fetch
     (
         `https://my-json-server.typicode.com/nikitabubriak/okr_lab4/orders`,
@@ -86,61 +85,57 @@ async function submitOrder ()
             body: JSON.stringify(order)
         }
     )
-    const response = await submitResponse.json();
-
-    console.log(response);
-
-    
-    
-    alert(`Order submission complete! ID ${response.id}`);
+    .then(response => response.json())
+    .then
+    (
+        (response) => 
+        {
+            console.log('1');
+            console.log(response);
+            alert(`Order submission complete! ID ${response.id}`);
             
-    const rootNode = document.getElementById('main-container');
-    rootNode.innerHTML = 
-    `
-    <p>Thank you for your purchase! This online shop was made for educational purposes only</p><br>
-    <p>Order ${response.id}</p>
-    <br><p>Details:</p><br>
-    <p>${response.name}</p>
-    <p>${response.email}</p>
-    <p>${response.phone}</p>
-    <p>${response.date}</p>
-    <p>${response.time}</p>
-    <p>${response.payment}</p>
-    <p>${response.card}</p>
-    <p>${response.name}</p>
-    <p>Order Total: ${response.total} ₴</p>
-    `;
-
-    history.replaceState(null, null, document.location.pathname + `#order/${response.id}`);
-    cartClear();
-    
-    //.then(response => response.json())
-    //.then(res => console.log(res));
-
-
-    // .then
-    // (
-    //     (response) => 
-    //     {
-    //         console.log(response);
+            console.log('2');
+            console.log(response);
+            const rootNode = document.getElementById('main-container');
+            rootNode.innerHTML = 
+            `
+            <p>Thank you for your purchase! This online shop was made for educational purposes only</p><br>
+            <p>Order ${response.id}</p>
+            <br><p>Details:</p><br>
+            <p>${response.name}</p>
+            <p>${response.email}</p>
+            <p>${response.phone}</p>
+            <p>${response.date}</p>
+            <p>${response.time}</p>
+            <p>${response.payment}</p>
+            <p>${response.card}</p>
+            <p>${response.name}</p>
+            <p>Order Total: ${response.total} ₴</p>
+            `;
+            console.log('3');
+            console.log(response);
+            cartClear();
+            history.replaceState(null, null, document.location.pathname + `#order/${response.id}`);
             
-            
-    //     }
-    // )
+            console.log('4');
+            console.log(response);
+            //window.location.hash += `/${response.id}`;
+            //setTimeout(function() { alert("Alert! This is test"); }, 3000);
+        }
+    )
     // .finally(() => 
     // {
         
     // });
-    //}
+    
+    //try
+    //{
     // catch (submissionError)
     // {
     //     alert("Error. Submission not successful");
     //     window.location.hash = '#';
     // }
-    
-    //window.location.hash += `/${response.id}`;
-    //setTimeout(function() { alert("Alert! This is test"); }, 3000);
-    
+
     //const content = await response.json();
     //console.log(content);
     //window.location.hash += `/${response.id}`;
